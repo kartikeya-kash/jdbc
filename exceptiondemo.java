c6870f40f94ff1bedbbcc69edb6192206de28757
Kartikeya <kartikeya.anjul@gmail.com>
2025-02-14 16:41:20 +0530

    try and catch example

---
+++ exceptiondemo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class exceptiondemo {
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        int dividend = input.nextInt();
+        int divisor = input.nextInt();
+        try{
+        int res =  (dividend/divisor);
+        System.out.println("result = "+ res);
+        }
+        catch(ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
